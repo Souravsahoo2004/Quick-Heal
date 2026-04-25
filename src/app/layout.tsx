69d25686cea95295f6fccd3338219d0660bbf8cf
sourav sahoo <souravsahoo72051@gmail.com>
2026-04-25 12:39:24 +0530

    add root layout fix

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Quick Heal",
+  description: "Healthcare App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
